efb42acbb44f389ceebd808f3cb33b4be3090e52
snapbuy <snapbuy@naver.com>
2021-12-11 22:20:12 +0900

    files

---
+++ FASTCAMPUS-react-troubleshoot-workshop/workshop-1/src/App.js
@@ -0,0 +1,8 @@
+import MatchPage from './pages/match/incomplete';
+// import MatchPage from './pages/match/complete';
+// import MatchPage from './pages/match/refactored-with-container';
+// import MatchPage from './pages/match/refactored-with-hooks';
+
+const App = () => <MatchPage />;
+
+export default App;
